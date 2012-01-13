5536b4a7dea8508e2d0e72c194923146c49b59b7
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2012-01-13 12:05:44 +0100

    Code modularization

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Responsivekit for web2py !
-## unifying my personal efforts for web2p2 responsiveness
+## unifying my personal efforts for web2py responsiveness
 
 Here you can find a comprehensive plugin for responsive design in web2py.
 It's not so somplete right now, but I planned adding more features.
 
 It's based on my previuos work (web2py-responsive-tables) which it incorporates and new features like responsive images.
+
+It's apckage like a plugin, so to use simply install it as any other plugin.
+
+### Features
+####Responsive Tables
+
+If you want to use responsive tables in a custom layout, you have to include this code:
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" type="text/css" href="{{=URL('static','plugin_layout_responsivetables/responsive-tables.css')}}" />
+	<script src="{{=URL('static','plugin_layout_responsivetables/responsive-tables.js')}}"></script>
+	<!--<![endif]-->
+
+It's important to include conditional directives for IE, because IE doesn't supports this responsive tables design, so it's disabled.
+
+Plugin works changing table's aspect on the fly when the resolution of target device is smaller than 700 pixels, it uses a combination of css media queries and javascript to accomplish the goal. If you want to change the resolution you have to edit both the css and the javascript (replacing 700 with your desired value).
+
+#### Responsive Images
+
+
+### Live demo
+You can find it [here](http://angelo.fluxflex.com/responsivekit)
--- models/plugin_responsivekit.py
+++ models/plugin_responsivekit.py
@@ -1,7 +1,39 @@
 from gluon.tools import PluginManager
 from gluon import current
 
+"""
+Responsivekit configuration:
+Here you can change configuration of responsivekit plugin.
+
+Service: PIL / src.sencha.io
+* PIL
+PIL is required to image resize on your site. 
+This works resizing the image on the fly and storing the resized image on disk for future requests.
+Images are resized only on multiples of 20px, so a dos attack cannot be accomplished, at least
+you can have some tens resized images for each original image.
+Please refer to PIL manual for explanation on setting pil_format, pil_quality, pil_optimize, and pil_size.
+
+* src.sencha.io
+Use the famous src.sencha.io service, so it works also on evironments in which PIL cannot be installed
+pil_* are not used when you choose src.sencha.io.
+
+screensize_filter
+It's used to choose wich element of DOM will be used for width reference.
+If you have your images in .container and you want your images width no more the .container width
+keep the default setting.
+
+screensize_controller, screensize_function
+They are used to choose which controller sill be called for keeping up with screen size information.
+Function should be:
+
+    from plugin_responsivekit import *
+    def screensize():
+        return set_screen_size()
+"""
 plugins = PluginManager('responsivekit', service='src.sencha.io',
+                                            screensize_filter = '.container',
+                                            screensize_controller = 'default',
+                                            screensize_function = 'screensize',
                                             pil_format='JPEG',
                                             pil_quality=50,
                                             pil_optimize=1,
